a7ea7eb9414c5856637ad443d53e09669a7f3423
anzbert <anzbert@gmail.com>
2026-04-03 13:29:50 +0800

    vite pre process

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import "./css/global.css";
 import App from "./app.svelte";
 import { mount } from "svelte";
 
-const app = mount(App, {
+const app: App = mount(App, {
   target: document.body,
   intro: true, // show animations on load
 });
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { mdsvex } from "mdsvex";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import mdsvexConfig from "./mdsvex.config.js";
-import sveltePreprocess from "svelte-preprocess";
+// import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
 export default {
@@ -8,5 +9,5 @@ export default {
 
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [sveltePreprocess(), mdsvex(mdsvexConfig)],
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 };
